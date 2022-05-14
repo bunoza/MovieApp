8ffe9c55727453108182806fac0ad70a018ffc79
bunoza <61355658+bunoza@users.noreply.github.com>
2022-05-14 14:38:16 +0200

    remove comments

--- MovieApp/Modules/TabView/TabViewCoordinator.swift
+++ MovieApp/Modules/TabView/TabViewCoordinator.swift
@@ -1,6 +1,9 @@
-// Urheberrechtshinweis: Diese Software ist urheberrechtlich geschützt. Das Urheberrecht liegt bei
-// Research Industrial Systems Engineering (RISE) Forschungs-, Entwicklungs- und Großprojektberatung GmbH,
-// soweit nicht im Folgenden näher gekennzeichnet.
+//
+//  TabViewCoordinator.swift
+//  MovieApp
+//
+//  Created by Domagoj Bunoza on 04.10.2021..
+//
 
 import Foundation
 import UIKit
